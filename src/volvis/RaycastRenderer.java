94d0ec7162df2c219b39785a5ea47e2df6703f9f
panos <panos@10.211.55.3>
2020-11-19 19:46:34 +0100

    initial commit

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -39,6 +39,7 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
      * Gradient information of the loaded volume.
      */
     private GradientVolume gradients = null;
+    //add a comment here
 
     /**
      * Reference to the GUI panel.
